3e8c52aef63de6f591815328d2bd3a989283b320
Eric C. Hill <eric@stochastic.com>
2017-01-14 23:40:21 -0700

    Lint

--- Parser.hs
+++ Parser.hs
@@ -1,4 +1,9 @@
-module Parser where
+module Parser (
+  numExpr,
+  module Syntax
+  ) where
+import Lexer
+import Syntax
 import System.IO
 import Control.Monad
 import Control.Monad.Identity
@@ -7,128 +12,59 @@ import Text.ParserCombinators.Parsec.Expr
 import Text.ParserCombinators.Parsec.Language
 import qualified Text.ParserCombinators.Parsec.Token as Token
 
-data NumExpr = NumConst Double
-             | NumVar String
-             | NumUnOp NUnOp NumExpr
-             | NumBinOp NBinOp NumExpr NumExpr
-             | NumTest BoolExpr NumExpr NumExpr
-             | NumFunc Function [NumExpr]
+numExpr = do
+  e <- numExpr'
+  e' <- optionMaybe ternary
+  case e' of
+    Nothing -> return e
+    Just (ifTrue, ifFalse) -> return $ CondExpr e ifTrue ifFalse
+  
+ternary = do
+  reservedOp "?"
+  ifTrue <- numExpr
+  colon
+  ifFalse <- numExpr
+  return (ifTrue, ifFalse)
 
-data BoolExpr = BoolConst Bool
-              | BoolNot BoolExpr
-              | BoolBinOp BBinOp BoolExpr BoolExpr
-              | BoolRelExpr RelExpr
+numExpr' = buildExpressionParser numTable numTerm <?> "numexpr"
 
-data RelExpr = RelTerm NumExpr | RelBinOp RelOp RelExpr RelExpr
-
-data Function = NamedFunc String
-              | NamelessFunc [String] NumExpr
-
-data NUnOp = Negate | Factorial
-
-data NBinOp = Add | Subtract | Multiply | Divide | Modulo | Pow
-
-data BBinOp = And | Or
-
-data RelOp = RelLT | RelLE | RelEQ | RelGE | RelGT | RelNE
-
-languageDef :: LanguageDef st
-languageDef =
-  let java = javaStyle in
-    java { Token.opStart = opStart emptyDef,
-           Token.opLetter = opLetter emptyDef,
-           Token.reservedNames = ["gcd", "lcm", "pi", "exp", "sqrt", "log",
-                                  "sin", "tan", "cos", "asin", "atan", "acos",
-                                  "sinh", "tanh", "cosh", "asinh", "atanh", "acosh",
-                                  "trunc", "round", "ceil", "floor",
-                                  "true", "false"],
-           Token.reservedOpNames = ["+", "-", "*", "/", "%", "^", "!",
-                                    "<", "<=", "==", ">=", ">", "!=",
-                                     "?", ":",
-                                     "&&", "||", "~"]
-         }
-
-type TokenParser st = Token.GenTokenParser String st Identity
-
---lexer :: TokenParser st
-lexer = Token.makeTokenParser languageDef
-
-identifier = Token.identifier lexer
-reserved   = Token.reserved   lexer
-reservedOp = Token.reservedOp lexer
-braces     = Token.braces     lexer
-colon      = Token.colon      lexer
-comma      = Token.comma      lexer
-commaSep1  = Token.commaSep1  lexer
-parens     = Token.parens     lexer
-float      = Token.float      lexer
-whitespace = Token.whiteSpace lexer
-
-
-numExpr :: Parser NumExpr
-numExpr = buildExpressionParser numTable numTerm <?> "numexpr"
-
-numTable = [ [Prefix  (reservedOp "-" >> return (NumUnOp Negate     ))            ]
-           , [Postfix (reservedOp "!" >> return (NumUnOp Factorial  ))            ]
-           , [Infix   (reservedOp "^" >> return (NumBinOp Pow       )) AssocRight ]
-           , [Infix   (reservedOp "*" >> return (NumBinOp Multiply  )) AssocLeft  ]
-           , [Infix   (reservedOp "/" >> return (NumBinOp Divide    )) AssocLeft  ]
-           , [Infix   (reservedOp "%" >> return (NumBinOp Modulo    )) AssocLeft  ]
-           , [Infix   (reservedOp "+" >> return (NumBinOp Add       )) AssocLeft  ]
-           , [Infix   (reservedOp "-" >> return (NumBinOp Subtract  )) AssocLeft  ] ]
+numTable = [ [Prefix  (reservedOp "-"  >> return (UnaryExpr  Negate    ))            ]
+           , [Postfix (reservedOp "!"  >> return (UnaryExpr  Factorial ))            ]
+           , [Infix   (reservedOp "^"  >> return (BinaryExpr Pow       )) AssocRight ]
+           , [Infix   (reservedOp "*"  >> return (BinaryExpr Multiply  )) AssocLeft  ]
+           , [Infix   (reservedOp "/"  >> return (BinaryExpr Divide    )) AssocLeft  ]
+           , [Infix   (reservedOp "%"  >> return (BinaryExpr Modulo    )) AssocLeft  ]
+           , [Infix   (reservedOp "+"  >> return (BinaryExpr Add       )) AssocLeft  ]
+           , [Infix   (reservedOp "-"  >> return (BinaryExpr Subtract  )) AssocLeft  ]
+           , [Infix   (reservedOp "<"  >> return (BinaryExpr RelLT     )) AssocNone  ]
+           , [Infix   (reservedOp "<=" >> return (BinaryExpr RelLE     )) AssocNone  ]
+           , [Infix   (reservedOp "==" >> return (BinaryExpr RelEQ     )) AssocNone  ]
+           , [Infix   (reservedOp ">=" >> return (BinaryExpr RelGE     )) AssocNone  ]
+           , [Infix   (reservedOp ">"  >> return (BinaryExpr RelGT     )) AssocNone  ]
+           , [Infix   (reservedOp "!=" >> return (BinaryExpr RelNE     )) AssocNone  ]
+           , [Prefix  (reservedOp "~"  >> return (UnaryExpr  Not       ))            ]
+           , [Infix   (reservedOp "&&" >> return (BinaryExpr And       )) AssocLeft  ]
+           , [Infix   (reservedOp "||" >> return (BinaryExpr Or        )) AssocLeft  ] ]
 
 numTerm =  parens numTerm2
-       <|> liftM NumVar identifier
-       <|> liftM NumConst float
+       <|> liftM Variable identifier
+       <|> liftM Constant float
 
-numTerm2 = funcCall <|> namelessFuncCall <|> numConditional <|> numExpr
+numTerm2 = funcCall <|> lambdaCall <|> numExpr
 
 funcCall = do
   name <- identifier
   args <- parens $ commaSep1 numExpr
-  return $ NumFunc (NamedFunc name) args
+  return $ FuncCall (FuncRef name) args
 
-namelessFuncCall = do
-  def <- braces namelessDef
+lambdaCall = do
+  def <- braces lambdaDef
   args <- parens $ commaSep1 numExpr
-  return $ NumFunc def args
+  return $ FuncCall def args
 
-namelessDef = do
+lambdaDef = do
   params <- parens $ commaSep1 identifier
   reservedOp ":"
   def <- numExpr
-  return $ NamelessFunc params def
+  return $ Lambda params def
 
-numConditional = do
-  test <- boolExpr
-  reservedOp "?"
-  ifTrue <- numExpr
-  reservedOp ":"
-  ifFalse <- numExpr
-  return $ NumTest test ifTrue ifFalse
-
-boolExpr :: Parser BoolExpr
-boolExpr = buildExpressionParser boolTable boolTerm <?> "boolexpr"
-
-boolTable = [ [Prefix (reservedOp "~"  >> return (BoolNot            ))           ]
-            , [Infix  (reservedOp "&&" >> return (BoolBinOp And      )) AssocLeft ]
-            , [Infix  (reservedOp "||" >> return (BoolBinOp Or       )) AssocLeft ] ]
-
-boolTerm = relation <|> parens boolExpr
-
-relation = do
-  expr <- relExpr
-  return $ BoolRelExpr expr
-
-relExpr :: Parser RelExpr
-relExpr = buildExpressionParser relTable relTerm <?> "relexpr"
-
-relTable = [ [Infix  (reservedOp "<"  >> return (RelBinOp RelLT )) AssocNone ]
-           , [Infix  (reservedOp "<=" >> return (RelBinOp RelLE )) AssocNone ]
-           , [Infix  (reservedOp "==" >> return (RelBinOp RelEQ )) AssocNone ]
-           , [Infix  (reservedOp ">=" >> return (RelBinOp RelGE )) AssocNone ]
-           , [Infix  (reservedOp ">"  >> return (RelBinOp RelGT )) AssocNone ]
-           , [Infix  (reservedOp "!=" >> return (RelBinOp RelNE )) AssocNone ] ]
-
-relTerm =  parens relExpr
-       <|> liftM RelTerm numExpr
--- Translate.hs
+++ Translate.hs
@@ -14,31 +14,31 @@ translate :: (VarStore a) => NumExpr -> TranslateError a
 translate (Constant x) = return $ Evaluator $ \_ _ -> return x
 
 translate (Variable name) =
-  return $ Evaluator $ \vars _ -> do
+  return $ Evaluator $ \vars _ ->
   case getVar name vars of
-    Just e -> (runEvaluator e) vars []
+    Just e -> runEvaluator e vars []
     Nothing -> throwError $ name ++ " is undefined."
 
 translate (UnaryExpr op e) = do
   func <- lookupUnary op
   e' <- translate e
-  return $ Evaluator $ \vars args -> liftA func ((runEvaluator e') vars args)
+  return $ Evaluator $ \vars args -> liftA func (runEvaluator e' vars args)
 
 translate (BinaryExpr op e1 e2) = do
   func <- lookupBinary op
   e1' <- translate e1
   e2' <- translate e2
   return $ Evaluator $ \vars args ->
-    liftA2 func ((runEvaluator e1') vars args) ((runEvaluator e2') vars args)
+    liftA2 func (runEvaluator e1' vars args) (runEvaluator e2' vars args)
 
 translate (CondExpr test e1 e2) = do
   test' <- translate test
   e1' <- translate e1
   e2' <- translate e2
   return $ Evaluator $ \vars args -> do
-    t1 <- (runEvaluator test') vars args
-    if t1 /= 0 then (runEvaluator e1') vars args
-      else (runEvaluator e2') vars args
+    t1 <- runEvaluator test' vars args
+    if t1 /= 0 then runEvaluator e1' vars args
+      else runEvaluator e2' vars args
 
 translate (FuncCall func args) = mapM translate args >>= translateFunc func
 
@@ -46,22 +46,14 @@ translate (FuncCall func args) = mapM translate args >>= translateFunc func
 translateFunc :: (VarStore a) => Function -> [Evaluator a] -> TranslateError a
 
 translateFunc (FuncRef name) args =
-  return $ Evaluator $ \vars _ -> do
+  return $ Evaluator $ \vars _ ->
     case getVar name vars of
-      Just func -> do
-        --args' <- evalArgs args vars
-        (runEvaluator func) vars args
-      Nothing -> throwError $ "Function " ++ name ++ " undefined."
+      Just func -> runEvaluator func vars args
+      Nothing   -> throwError $ "Function " ++ name ++ " undefined."
 
 translateFunc (Lambda params expr) args = do
   expr' <- translate expr
-  return $ Evaluator $ \vars _ -> do
-    let vars' = foldl (\vs (k, v) -> setVar k v vs) vars $ zip params args in
-      (runEvaluator expr') vars' []
-
-
-evalArgs :: (VarStore a) => [Evaluator a] -> a -> EvalError [Double]
-evalArgs args vars = mapM (\arg -> (runEvaluator arg) vars []) args
-
-constEvaluator :: (VarStore a) => Double -> Evaluator a
-constEvaluator x = Evaluator $ \_ _ -> return x
+  return $ Evaluator $ \vars _ ->
+    let vars'
+          = foldl (\vs (k, v) -> setVar k v vs) vars $ zip params args
+      in runEvaluator expr' vars' []
